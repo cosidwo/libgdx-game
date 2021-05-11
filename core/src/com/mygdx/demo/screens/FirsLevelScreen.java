ae6c6b58f05c30a96893a816cec09aae8f17c954
cosidwo <piotr.kosidlo1@gmail.com>
2021-05-11 11:04:46 +0200

    Add assets and implement menu screen
    Added assets for background of the menu screen and 4 button assests used in menu screen.
    
    Implemented the menu screen:
    - added background and button textures
    - implemented button change on mouse hover
    - implemented mouse click on button start (redirects to level 1 screen)
    - implemented mouse click on button exit (exits the game)

---
+++ core/src/com/mygdx/demo/screens/FirsLevelScreen.java
@@ -0,0 +1,12 @@
+package com.mygdx.demo.screens;
+import com.mygdx.demo.LibGDXGame;
+import com.mygdx.demo.screens.AbstractScreen;
+
+public class FirsLevelScreen extends AbstractScreen{
+
+    public FirsLevelScreen(LibGDXGame libGDXGame) {
+        super(libGDXGame);
+    }
+
+
+}
