9bdb0edb608294245299756a12d462f1da16c38a
cosidwo <piotr.kosidlo1@gmail.com>
2021-08-27 10:48:13 +0200

    Update tilemap
    Changed cherry sprite

---
+++ core/assets/sunnyland_map/cherry.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="cherry" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="cherry-3-new.png" width="19" height="19"/>
+</tileset>
